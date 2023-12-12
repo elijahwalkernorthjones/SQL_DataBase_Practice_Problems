22effdb2958a6de5e6108065c6fda266aa386826
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2023-12-11 19:35:14 -0500

    initial commit for these SQL practice problems

---
+++ air_traffic.sql
@@ -0,0 +1,114 @@
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS air_traffic;
+
+-- Create a new database
+CREATE DATABASE air_traffic;
+
+-- Connect to the new database
+\c air_traffic;
+
+-- Create table for Airlines
+CREATE TABLE airlines
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+-- Insert data into the Airlines table
+INSERT INTO airlines (name) VALUES
+  ('United'),
+  ('British Airways'),
+  ('Delta'),
+  ('TUI Fly Belgium'),
+  ('Air China'),
+  ('American Airlines'),
+  ('Avianca Brasil');
+
+-- Create table for Passengers
+CREATE TABLE passengers
+(
+  id SERIAL PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL
+);
+
+-- Insert data into the Passengers table
+INSERT INTO passengers (first_name, last_name) VALUES
+  ('Jennifer', 'Finch'),
+  ('Thadeus', 'Gathercoal'),
+  ('Sonja', 'Pauley'),
+  ('Waneta', 'Skeleton'),
+  ('Berkie', 'Wycliff'),
+  ('Alvin', 'Leathes'),
+  ('Cory', 'Squibbes');
+
+-- Create table for Cities
+CREATE TABLE cities
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+-- Insert data into the Cities table
+INSERT INTO cities (name) VALUES
+  ('Washington DC'),
+  ('Tokyo'),
+  ('Los Angeles'),
+  ('Seattle'),
+  ('Paris'),
+  ('Dubai'),
+  ('New York'),
+  ('Cedar Rapids'),
+  ('Charlotte'),
+  ('Sao Paolo');
+
+-- Create table for Countries
+CREATE TABLE countries
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+-- Insert data into the Countries table
+INSERT INTO countries (name) VALUES
+  ('United States'),
+  ('Japan'),
+  ('United States'),
+  ('United States'),
+  ('France'),
+  ('UAE'),
+  ('United States'),
+  ('United States'),
+  ('United States'),
+  ('Brazil'),
+  ('Chile');
+
+-- Create the main table for Tickets
+CREATE TABLE tickets
+(
+  id SERIAL PRIMARY KEY,
+  passenger_id INTEGER REFERENCES passengers(id),
+  seat TEXT NOT NULL,
+  departure TIMESTAMP NOT NULL,
+  arrival TIMESTAMP NOT NULL,
+  airline_id INTEGER REFERENCES airlines(id),
+  from_city_id INTEGER REFERENCES cities(id),
+  from_country_id INTEGER REFERENCES countries(id),
+  to_city_id INTEGER REFERENCES cities(id),
+  to_country_id INTEGER REFERENCES countries(id)
+);
+
+-- Insert data into the Tickets table
+INSERT INTO tickets
+  (passenger_id, seat, departure, arrival, airline_id, from_city_id, from_country_id, to_city_id, to_country_id)
+VALUES
+  (1, '33B', '2018-04-08 09:00:00', '2018-04-08 12:00:00', 1, 1, 1, 4, 1),
+  (2, '8A', '2018-12-19 12:45:00', '2018-12-19 16:15:00', 2, 2, 2, 3, 2),
+  (3, '12F', '2018-01-02 07:00:00', '2018-01-02 08:03:00', 3, 3, 1, 9, 1),
+  (1, '20A', '2018-04-15 16:50:00', '2018-04-15 21:00:00', 3, 4, 1, 10, 6),
+  (4, '23D', '2018-08-01 18:30:00', '2018-08-01 21:50:00', 4, 5, 5, 11, 7),
+  (2, '18C', '2018-10-31 01:15:00', '2018-10-31 12:55:00', 5, 6, 6, 12, 5),
+  (5, '9E', '2019-02-06 06:00:00', '2019-02-06 07:47:00', 1, 7, 1, 8, 1),
+  (6, '1A', '2018-12-22 14:42:00', '2018-12-22 15:56:00', 6, 8, 1, 9, 1),
+  (5, '32B', '2019-02-06 16:28:00', '2019-02-06 19:18:00', 6, 9, 1, 13, 1),
+  (7, '10D', '2019-01-20 19:30:00', '2019-01-20 22:45:00', 7, 10, 10, 14, 11);
