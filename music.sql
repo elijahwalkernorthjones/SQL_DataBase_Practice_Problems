4b7548102448a63bf3fe2da572680ec958fa46d2
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2023-12-11 19:59:15 -0500

    second problem figured out

--- music.sql
+++ music.sql
@@ -1,33 +1,75 @@
--- from the terminal run:
--- psql < music.sql
-
+-- Drop the database if it exists
 DROP DATABASE IF EXISTS music;
 
+-- Create a new database
 CREATE DATABASE music;
 
-\c music
+-- Connect to the new database
+\c music;
 
+-- Create table for Artists
+CREATE TABLE artists
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+-- Insert data into the Artists table
+INSERT INTO artists (name) VALUES
+  ('Hanson'),
+  ('Queen'),
+  ('Mariah Carey'),
+  ('Boyz II Men'),
+  ('Lady Gaga'),
+  ('Bradley Cooper'),
+  ('Nickelback'),
+  ('Jay Z'),
+  ('Alicia Keys'),
+  ('Katy Perry'),
+  ('Juicy J'),
+  ('Maroon 5'),
+  ('Christina Aguilera'),
+  ('Avril Lavigne'),
+  ('Destiny''s Child');
+
+-- Create table for Producers
+CREATE TABLE producers
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+-- Insert data into the Producers table
+INSERT INTO producers (name) VALUES
+  ('Dust Brothers'),
+  ('Stephen Lironi'),
+  ('Roy Thomas Baker'),
+  ('Walter Afanasieff'),
+  ('Benjamin Rice'),
+  ('Rick Parashar'),
+  ('Al Shux'),
+  ('Max Martin'),
+  ('Cirkut'),
+  ('Shellback'),
+  ('Benny Blanco'),
+  ('The Matrix'),
+  ('Darkchild');
+
+-- Create the main table for Songs
 CREATE TABLE songs
 (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
   duration_in_seconds INTEGER NOT NULL,
   release_date DATE NOT NULL,
-  artists TEXT[] NOT NULL,
+  artist_ids INTEGER REFERENCES artists(id) NOT NULL,
   album TEXT NOT NULL,
-  producers TEXT[] NOT NULL
+  producer_ids INTEGER REFERENCES producers(id) NOT NULL
 );
 
-INSERT INTO songs
-  (title, duration_in_seconds, release_date, artists, album, producers)
+-- Insert data into the Songs table
+INSERT INTO songs (title, duration_in_seconds, release_date, artist_ids, album, producer_ids)
 VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+  ('MMMBop', 238, '1997-04-15', 1, 'Middle of Nowhere', 1),
+  ('Bohemian Rhapsody', 355, '1975-10-31', 2, 'A Night at the Opera', 3);
+
